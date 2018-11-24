6e95b537bcc20fb3d2571f5b91fdeaf1569044ea
aserfend <liuchangm7j@vip.qq.com>
2018-11-24 18:49:24 +0800

    优化总览识别

--- xsp/src/Building/pandect_Other.lua
+++ xsp/src/Building/pandect_Other.lua
@@ -1,9 +1,33 @@
+function pandect:DoResearch(targetPos,param)
+	ShowInfo.RunningInfo("<研究所>")
+	if param==1 then
+		tap(targetPos.x,targetPos.y)
+	else if param==2 then
+			tap(targetPos.x,targetPos.y)
+			sleep(1000)
+			Building.building:ResearchBegin()
+		end
+	end
+end
+--1:可直接领取 2:需要研究
 function pandect:CheckIfResearch()
-	
+	local result=0
+	if self:PanelIsGreen(1,1) then
+		result=1
+	else
+		if self:PanelIsBlue(1,1) then
+			result=2
+		else
+			return false
+		end
+	end
+	return function()
+		self:DoResearch(self:PanelPos(1,1),result)
+	end
 end
 
 function pandect:GetSupply(targetPos)
-	ShowInfo.RunningInfo("获取空投补给")
+	ShowInfo.RunningInfo("<空投补给>")
 	tap(targetPos.x,targetPos.y)
 	sleep(500)
 	tap(360,830)
@@ -25,26 +49,21 @@ end
 -- -1:无 0:需要治疗 1:等待治疗 2:可收取
 function pandect:CheckIfTreat()
 	local result=0
-	local point = screen.findColor(Rect(525, 458, 166, 38), 
-"0|0|0xe26f30,-30|-2|0xbd4929,-101|-4|0xbb4928",
-95, screen.PRIORITY_DEFAULT)
-	if point.x>0 then
+	if self:PanelIsOrange(2,1) then
 		result=0
 	else
-		point = screen.findColor(Rect(525, 458, 166, 38), 
-"0|0|0x45b538,-10|0|0x44b436,-100|-3|0x40b235",
-95, screen.PRIORITY_DEFAULT)
-		if point.x>0 then
+		if self:PanelIsGreen(2,1) then
 			result=2
 		else
 			return false
 		end
 	end
 	return function() 
-		self:DoTreat(point,result)
+		self:DoTreat(self:PanelPos(2,1),result)
 	end
 end
 function pandect:DoTreat(targetPos,param)
+	ShowInfo.RunningInfo("<维修部队>")
 	if param==2 then
 		tap(targetPos.x,targetPos.y)--仅收取
 		sleep(1000)
---
+++ xsp/src/Building/pandect_Panel.lua
@@ -0,0 +1,33 @@
+local panelPosX={
+	[1]=296,[2]=507
+}
+local panelPosYBegin=352
+local panelWidth=200
+local panelHeight=150
+
+local panelGreen="0|0|0x38a633,-102|2|0x3fae38,-52|9|0x42b33a"--实验室绿色
+local panelBlue="0|0|0x516dc9,49|11|0x5773ce,94|3|0x4e6bc7"--实验室蓝色
+local panelOrange="0|0|0xe26f30,-30|-2|0xbd4929,-101|-4|0xbb4928"--维修橙色
+function pandect:PanelPos(indexX,indexY)
+	return {x=panelPosX[indexX]+10,y=panelPosYBegin+160*(indexY-1)+10}
+end
+function pandect:PanelIsGreen(indexX,indexY)
+	return self:PanelCheckColor(indexX,indexY,panelGreen)
+end
+function pandect:PanelIsBlue(indexX,indexY)
+	return self:PanelCheckColor(indexX,indexY,panelBlue)
+end
+function pandect:PanelIsOrange(indexX,indexY)
+	return self:PanelCheckColor(indexX,indexY,panelOrange)
+end
+function pandect:PanelCheckColor(indexX,indexY,colorInfo)
+	return self:PanelCheckRangeColor(
+	panelPosX[indexX],panelPosYBegin+160*(indexY-1),
+	panelWidth,panelHeight,colorInfo)
+end
+function pandect:PanelCheckRangeColor(x,y,w,h,colorInfo)
+	local point = screen.findColor(Rect(x, y, w, h), 
+	colorInfo,
+	95, screen.PRIORITY_DEFAULT)
+	return point.x>0
+end
