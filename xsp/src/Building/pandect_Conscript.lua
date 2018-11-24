ec8e1e00f6b31ccca792949485a9c3efbf8a2969
aserfend <liuchangm7j@vip.qq.com>
2018-11-24 18:49:07 +0800

    init

---
+++ xsp/src/Building/pandect_Conscript.lua
@@ -0,0 +1,39 @@
+
+function pandect:CheckIfTank()
+	return false
+end
+function pandect:CheckIfAir()
+	return false
+end
+function pandect:CheckIfSoldier()
+	return false
+end
+function pandect:CheckIfChariot()
+	return false
+end
+function pandect:CheckIfDefence()
+	if self:PanelIsGreen(1,3) then
+	
+	else
+		if self:PanelIsBlue(1,3) then
+		
+		end
+	end
+end
+
+
+function pandect:ProduceTank()
+
+end
+function pandect:ProduceAir()
+
+end
+function pandect:ProduceSoldier()
+
+end
+function pandect:ProduceChariot()
+
+end
+function pandect:ProduceDefence()
+
+end
---
+++ xsp/src/Building/pandect_Expedition.lua
@@ -0,0 +1,3 @@
+function pandect:ExpeditionCheck()
+
+end
