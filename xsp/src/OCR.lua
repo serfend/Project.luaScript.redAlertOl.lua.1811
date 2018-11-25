94e0a4228e3d0d09f1101ff6b10b9268efc055d8
aserfend <liuchangm7j@vip.qq.com>
2018-11-25 15:07:43 +0800

    新增出征等级识别和变换

--- xsp/src/OCR.lua
+++ xsp/src/OCR.lua
@@ -1,57 +1,30 @@
 OCR = {
-	ocrNum,ocrNumBold, msg = nil,nil,nil
+	ocrMapPos,ocrNumBold, msg = nil,nil,nil,
 }--初始化
 function OCR:showError()
 	dialog("加载字库错误:"..self.msg)
 	lua_exit()
 end
+local tessocr=require('tessocr_3.05.02')
 function OCR:new (o)
     o = o or {}
     setmetatable(o, self)
     self.__index = self
---	self.ocrNum,self.msg=createOCR({
---		type = "tesseract",
---		path = "res/", -- 自定义字库暂时只能放在脚本res/目录下
---		lang = "num", -- 使用生成的num.traineddata文件
+	self.ocrMapPos,self.msg=tessocr.create({
+		path = "res/", -- 自定义字库暂时只能放在脚本res/目录下
+		lang = "num", -- 使用生成的num.traineddata文件
 		
---	})
---	if self.ocrNum==nil then
---		self:showError()
---	end
---	self.ocrNumBold,self.msg=createOCR({
---		type = "tesseract",
---		path = "res/", 
---		lang = "numBold" 
---	})
---	if self.ocrNumBold==nil then
---		self:showError()
---	end
+	})
+	
+	if self.ocrMapPos==nil then
+		self:showError()
+	end
     return o
 end
-function OCR:GetNum(x1,y1,x2,y2,foreColor)
+function OCR:GetMapPos(x,y,w,h,foreColor)
 	local rect = {x1, y1, x2, y2}
 	local diff = {foreColor or "0xffffff-0x0f0f0f"}
-	local code, text = self.ocrNum:getText({
-		rect = rect,
-		diff = diff,
-	})
-	return code ,text
-end
-function OCR:GetNumBold(x1,y1,x2,y2,foreColor)
-	local rect = {x1, y1, x2, y2}
-	local diff = {foreColor or "0xffffff-0x0f0f0f"}
-	local code, text = self.ocrNumBold:getText({
-		rect = rect,
-		diff = diff,
-	})
-	return code ,text
-end
-function OCR:GetString(x1,y1,x2,y2,foreColor)
-	local rect = {x1, y1, x2, y2}
-	local diff = {foreColor or "0xffffff-0x0f0f0f"}
-	local code, text = self.ocrString:getText({
-		rect = rect,
-		diff = diff,
-	})
+	local code, text = self.ocrMapPos:getText(Rect(x,y,w,h),diff)
+	
 	return code ,text
 end
---
+++ xsp/src/OCR/OCR.lua
@@ -0,0 +1,53 @@
+OcrInfo={
+	fore={
+		mapPos="0xe1e0e0-0xa0a0a0",
+		mapTargetRank="0xa99956-0xa0a022"
+	}
+}
+function OcrInfo:new (o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+function OcrInfo:GetMapTargetRank()
+	local code,result=ocr:GetMapPos(255,1125,61,20,self.fore.mapTargetRank)
+	return result
+end
+
+
+--@summary:需保证当前在地图场景下
+function OcrInfo:GetMapPos()
+	local judgeY=155
+	screen.keep(true)
+	local x1,x2=self:GetMapPosRange(judgeY)
+	x1=x1+2
+	x2=x2-2
+	judgeY=judgeY-5
+	local code,result=ocr:GetMapPos(x1,judgeY,x2-x1,25,self.fore.mapPos)
+	screen.keep(false)
+	return result
+end
+--@summary:通过识别白色区域获取坐标开始点和结束点
+function OcrInfo:GetMapPosRange(judgeY)
+	local beginX=364
+	local endX=490
+	
+	local resultX1=0
+	local resultX2=0
+	for nowX=beginX,endX do
+		local r,g,b=screen.getRGB(nowX,judgeY)
+		if r>100 and g>100 and b>100 then
+			resultX1=nowX
+			break
+		end
+	end
+	for nowX=endX,beginX,-1 do
+		local r,g,b=screen.getRGB(nowX,judgeY)
+		if r>100 and g>100 and b>100 then
+			resultX2=nowX
+			break
+		end
+	end
+	return resultX1,resultX2
+end
