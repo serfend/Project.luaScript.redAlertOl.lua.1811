14e902516f104c9d7ffd8fecbd6c489b19eec581
aserfend <liuchangm7j@vip.qq.com>
2018-12-05 22:00:14 +0800

    优化建筑寻址

--- xsp/src/SettingBase/Screen.lua
+++ xsp/src/SettingBase/Screen.lua
@@ -1,13 +1,13 @@
 CityMap={
 	建筑工厂={x=0,y=-100,Priority=4},
 	集结指挥部={x=-620,y=-35,Priority=1},
-	联盟大厦={x=-820,y=120,Priority=3},
+	联盟大厦={x=-820,y=100,Priority=3},
 	指挥中心={x=-1780,y=900,Priority=3},
-	战车工厂={x=-640,y=600,Priority=1},
-	坦克工厂={x=-900,y=780,Priority=1},
+	战车工厂={x=-600,y=600,Priority=1},
+	坦克工厂={x=-900,y=760,Priority=1},
 	城防工厂={x=-1300,y=440,Priority=1},
-	兵营={x=-300,y=780,Priority=1},
-	空指部={x=-600,y=935,Priority=1},
+	兵营={x=-280,y=760,Priority=1},
+	空指部={x=-590,y=905,Priority=1},
 	作战实验室={x=-120,y=420,Priority=2},
 	仓库={x=400,y=500,Priority=5},
 	光棱塔左={x=-10,y=1000,Priority=7},
@@ -16,6 +16,7 @@ CityMap={
 	贸易中心={x=-850,y=-220,Priority=7},
 	军需处={x=-1190,y=320,Priority=7},
 	特惠商人={x=120,y=320,Priority=-1},
+	军需处={x=-1080,y=340,Priority=-1},
 }
 
 for k,v in pairs(CityMap) do
